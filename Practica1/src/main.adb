a7a6e32257ea799fc178c76ea261fd15ed3a748f
Ignacio Aguilera Gómez <aguileragomezignacio@gmail.com>
2019-03-02 22:46:14 +0100

    Simulacion de la planta casi terminada, falta arreglar algunos errores y comentar el codigo.

--- Practica1/src/main.adb
+++ Practica1/src/main.adb
@@ -1,43 +1,48 @@
 with Ada.Text_IO;
-use Ada.Text_IO;
+with SistemaReal;
 
 procedure Main is
 
-   beta:float:=0.13;
-   Leq: float:=15;
-   h:   float:=4;
-   c:   float:=1080000;
-   Cp:  flaot:=4190;
-   rho: float:=975;    --p
+    maxRow: integer:=15;
+    maxCol: integer:=3;
 
-   maxRow:integer:=15;
-   maCol:integer:=3;
+    type inputData is array(1..maxRow, 1..maxCol) of float;
 
-   type data is array(1..maxRow, 1..maxCol) of float;
+    dataTable:inputData;
 
-   data(1)  :=(600); data(2) :=21.0; data(3) :=20.0;
-   data(4)  :=(604); data(5) :=21.0; data(6) :=19.6;
-   data(7)  :=(610); data(8) :=21.1; data(9) :=19.8;
-   data(10) :=(620); data(11):=21.0; data(12):=20.0;
-   data(13) :=(580); data(14):=21.0; data(15):=20.0;
-   data(16) :=(530); data(17):=21.0; data(18):=20.5;
-   data(19) :=(450); data(20):=21.0; data(21):=20.0;
-   data(22) :=(420); data(23):=21.2; data(24):=20.0;
-   data(25) :=(460); data(26):=21.0; data(27):=21.0;
-   data(28) :=(400); data(29):=21.0; data(30):=20.0;
-   data(31) :=(450); data(32):=21.0; data(33):=19.5;
-   data(34) :=(480); data(35):=21.3; data(36):=19.0;
-   data(37) :=(400); data(38):=21.0; data(39):=19.5;
-   data(40) :=(400); data(41):=21.0; data(42):=20.0;
-   data(43) :=(350); data(44):=21.0; data(45):=21.0;
+    od:SistemaReal.outputData;
 
 begin
 
-   For_Loop:
-      for k in Integer range 0..14 loop
+    dataTable:=(1 =>(600.0, 21.0, 20.0),
+		2 =>(604.0, 21.0, 19.6),
+		3 =>(610.0, 21.1, 19.8),
+		4 =>(620.0, 21.0, 20.0),
+		5 =>(580.0, 21.0, 20.0),
+		6 =>(530.0, 21.0, 20.5),
+		7 =>(450.0, 21.0, 20.0),
+		8 =>(420.0, 21.2, 20.0),
+		9 =>(460.0, 21.0, 21.0),
+		10=>(400.0, 21.0, 20.0),
+		11=>(450.0, 21.0, 19.5),
+		12=>(480.0, 21.3, 19.0),
+		13=>(400.0, 21.0, 19.5),
+		14=>(400.0, 21.0, 20.0),
+		15=>(350.0, 21.0, 21.0));
 
-        Put_Line(k'Img);
+    For_Loop:
+    for k in Integer range 1..maxRow loop
+	od:=SistemaReal.Planta(k, dataTable(k, 1), dataTable(k, 2), dataTable(k, 3));
+    end loop For_Loop;
 
-      end loop For_Loop;
+    -- Print for loop
+    For_LoopI:
+    for i in Integer range 1..maxRow loop
+	For_LoopJ:
+	for j in Integer range 1..maxCol loop
+	    ada.Text_IO.Put_Line(float'Image(dataTable(i, j)));
+	end loop For_LoopJ;
+	Ada.Text_IO.Put_Line("-----");
+    end loop For_LoopI;
 
 end Main;
--- Practica1/src/sistemareal.adb
+++ Practica1/src/sistemareal.adb
@@ -1,7 +1,90 @@
-package body SistemaReal is
+package body sistemareal is
 
-begin
+    sd:sensorsData;
+    
+    i :integer:=0;
+    
+    function Planta (t_k : integer; t_sr1, t_st4, t_st3 : float) return outputData is
+      
+	od:outputData;
+	
+	m_st1   :float;
+	m_st1_1 :float;
+	m_st2   :float;
+	m_st2_1 :float;
+	m_st3   :float;
+	m_st4   :float;
+	m_sc1   :float;
+	m_sc2   :float;
+	m_sr1   :float;
+	m_sd1   :float;
+	m_tt    :float;
+
+    begin
+	
+	if t_k < 2 then    -- Valores por defecto
+	    m_st1_1:=50.0;
+	    m_st2_1:=60.0;
+	else
+	    m_st1_1:=sd(t_k-1, St1);
+	    m_st2_1:=sd(t_k-1, St2);
+	end if;
+	    
+	m_st3:=t_st3;
+	m_st4:=t_st4;
+	m_sc1:=20.0;
+	m_sc2:=400.0;
+	m_sr1:=t_sr1;
+	m_sd1:=sd(t_k, Sd1);
+
+	m_tt:=(m_st1_1 + m_st2_1) / 2.0;
+	
+	m_st2:=m_st1_1 +
+	  (((beta * Leq * m_sr1)-(h * (m_tt - m_st4))) *
+          (c / (m_sc1 * Cp * rho)));
+
+	m_st1:=m_st2 - 11.0;
+
+		    
+	ada.text_io.Put_Line(float'Image(sd(t_k, St1)));
+	
+	od:=(m_st1, m_st2, m_st3, m_st4, 20.0, 400.0, m_sr1, m_sd1);
+	
+	For_Loop:
+	for s in sensores'Range loop      
+	    sd(t_k, s):=od(i);
+	    i:=i+1;
+	end loop For_Loop;	
+	
+	return od;
    
-   -- Todas las formulas
-
-end SistemaReal;
+    end Planta;
+    
+begin
+    
+    -- Inicializando la matriz de datos de sensores
+    For_LoopI:
+    for i in Integer range 1..maxRow loop
+	For_LoopJ:
+	for j in sensores'Range loop
+	    sd(i, j):=0.0;
+	end loop For_LoopJ;
+    end loop For_LoopI;
+	    
+--    sd:=(1  => (0.0, 0.0, 0.0, 0.0, 20.0, 400.0, 0.0, 0.0),
+--	2  => (0.0, 0.0, 0.0, 0.0, 20.0, 400.0, 0.0, 0.0),
+--	3  => (0.0, 0.0, 0.0, 0.0, 20.0, 400.0, 0.0, 0.0),
+--	4  => (0.0, 0.0, 0.0, 0.0, 20.0, 400.0, 0.0, 0.0),
+--	5  => (0.0, 0.0, 0.0, 0.0, 20.0, 400.0, 0.0, 0.0),
+--	6  => (0.0, 0.0, 0.0, 0.0, 20.0, 400.0, 0.0, 0.0),
+--	7  => (0.0, 0.0, 0.0, 0.0, 20.0, 400.0, 0.0, 0.0),
+--	8  => (0.0, 0.0, 0.0, 0.0, 20.0, 400.0, 0.0, 0.0),
+--	9  => (0.0, 0.0, 0.0, 0.0, 20.0, 400.0, 0.0, 0.0),
+--	10 => (0.0, 0.0, 0.0, 0.0, 20.0, 400.0, 0.0, 0.0),
+--	11 => (0.0, 0.0, 0.0, 0.0, 20.0, 400.0, 0.0, 0.0),
+--	12 => (0.0, 0.0, 0.0, 0.0, 20.0, 400.0, 0.0, 0.0),
+--	13 => (0.0, 0.0, 0.0, 0.0, 20.0, 400.0, 0.0, 0.0),
+--	14 => (0.0, 0.0, 0.0, 0.0, 20.0, 400.0, 0.0, 0.0),
+--	15 => (0.0, 0.0, 0.0, 0.0, 20.0, 400.0, 0.0, 0.0));
+   
+end sistemareal;
