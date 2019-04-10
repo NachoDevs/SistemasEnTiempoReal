fc4bac1abe3fd0680e9b2cb08c38495ffea3c0bc
nachodevs <nachodevelopments@gmail.com>
2019-04-10 16:39:39 +0200

    Comentando la practica 2, y solucionando un error con la ejecucion de los modulos.

--- Practica2/src/mainconcurrente.adb
+++ Practica2/src/mainconcurrente.adb
@@ -11,28 +11,32 @@ procedure mainConcurrente is
     j : integer := 0;     -- Variable auxiliar para el bucle del Modulo de Destilacion
     k : integer := 0;     -- Variable auxiliar para el bucle del Sistema de Seguridad
 
-    Tiempo1, Tiempo2, Tiempo3 : Time;
-    Limite1, Limite2, Limite3 : Time;
-    C1      : Time_Span := Milliseconds (10);
-    C2      : Time_Span := Milliseconds (10);
-    T1      : Time_Span := Milliseconds (100);
-    T2      : Time_Span := Milliseconds (200);   
-    T3      : Time_Span := Milliseconds (50);   
+    Tiempo1, Tiempo2, Tiempo3 : Time;            -- Variable para controlar el tiempo de cada modulo
+    Limite1, Limite2, Limite3 : Time;            -- Variable para almacenar el limite temporal de cada modulo
+    C1      : Time_Span := Milliseconds (10);    -- Variable para almacenar el tiempo de computo de un modulo
+    C2      : Time_Span := Milliseconds (10);    -- Variable para almacenar el tiempo de computo de un modulo
+    T1      : Time_Span := Milliseconds (100);   -- Variable para almacenar el de periodo de un modulo
+    T2      : Time_Span := Milliseconds (200);   -- Variable para almacenar el de periodo de un modulo   
+    T3      : Time_Span := Milliseconds (50);    -- Variable para almacenar el de periodo de un modulo   
     
     task type CS;
     type REF_CS is access CS;
     
+    -- Tarea del campo solar
     task body CS is
     begin
 	Tiempo1 := clock;
 	for i in 1 .. reps loop
 	    delay until (Tiempo1);
+	    -- Añadimos el timepo de computo
 	    limite1 := Tiempo1 + C1;
 	    select
 		delay until (limite1);
 	    then abort
+		-- Ejecutamos los pasos que sigue el campo solar
 		Modulos.CampoSolar;
 	    end select;
+	    -- Añadimos el periodo
 	    Tiempo1 := Tiempo1 + T1;
 	end loop;
     end;
@@ -45,12 +49,15 @@ procedure mainConcurrente is
 	Tiempo2 := clock;
 	for i in 1 .. reps loop
 	    delay until (Tiempo2);
+	    -- Añadimos el timepo de computo
 	    limite2 := Tiempo2 + C2;
 	    select
 		delay until (limite2);
 	    then abort
-		Modulos.CampoSolar;
+		-- Ejecutamos los pasos que sigue el modulo de destilacion
+		Modulos.ModuloDestilacion;
 	    end select;
+	    -- Añadimos el periodo
 	    Tiempo2 := Tiempo2 + T2;
 	end loop;
     end;
@@ -67,8 +74,10 @@ procedure mainConcurrente is
 	    select
 		delay until (limite3);
 	    then abort
-		Modulos.CampoSolar;
+		-- Ejecutamos los pasos que sigue el sistema de seguridad
+		Modulos.SistemaDeSeguridad;
 	    end select;
+	    -- Añadimos el periodo
 	    Tiempo3 := Tiempo3 + T3;
 	end loop;
     end;
--- Practica2/src/mainsecuencial.adb
+++ Practica2/src/mainsecuencial.adb
@@ -5,15 +5,19 @@ use Ada.Text_IO;
 
 procedure mainSecuencial is
 
-    i :integer := 0;
+    reps : integer := 10; -- Variable que indica el numero de repeticiones del bucle 
+
+    i    : integer :=  0; -- Variable auxiliar para el bucle del numero de repeticiones
     
 begin
    
-    for i in 1 .. 10 loop
+    for i in 1 .. reps loop
     
 	modulos.CampoSolar;        -- 10 ms
 	modulos.ModuloDestilacion; -- 10 ms
 	
+	-- Como suponemos que cada iteracion del bucle son 10ms, y sabemos que 
+	-- el sistema de seguridad se ejecuta cada 50ms, lo llamamos cada 5 iteraciones
 	if (i mod 5 = 0) then
 	    modulos.SistemaDeSeguridad; -- 50 ms
 	end if;
